3e7595ffbcdb1423fff97df84066cc3db0f789a7
coder.zhang <coder.zhang@accentrix.com>
2024-12-26 13:25:20 +0800

    init project

---
+++ src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="main">
+    <view
+     class="aaa">123
+      <view class="bbb">123</view>
+    </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+// <style lang="scss">
+// .main {
+//   .aaa {
+//     color: red;
+//     .bbb {
+//       color: blue;
+//     }
+//   }
+// }
+// </style>
